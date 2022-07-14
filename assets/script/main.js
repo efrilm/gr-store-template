5ca2e96a94e857a491978201c9467c8abfd340f7
Efril Mulanda <efril.mulanda@gmail.com>
2022-07-14 12:10:23 +0700

    header

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,16 +4,32 @@
     "use strict";
     $('select').niceSelect();
 
-    // 02. Mobile Menu Js
+    // Mobile Menu Js
     $('#mobile-menu').meanmenu({
         meanMenuContainer: '.mobile-menu',
         meanScreenWidth: "991",
-        meanExpand: ['<i class="fal fa-plus"></i>'],
+        meanExpand: ['<i class="fa fa-plus"></i>'],
     });
     $('.category-toggle-btn').on('click', function () {
         $('.category-menu').slideToggle(500);
     });
     $('.category-toggle-btn-2').on('click', function () {
         $('.side-menu').slideToggle(500);
+    });
+
+    //  Sidebar Js
+    $(".offcanvas-toggle-btn").on("click", function () {
+        $(".offcanvas-area").addClass("opened");
+        $(".body-overlay").addClass("opened");
+    });
+    $(".offcanvas-close-button").on("click", function () {
+        $(".offcanvas-area").removeClass("opened");
+        $(".body-overlay").removeClass("opened");
+    });
+
+    //  Body overlay Js
+    $(".body-overlay").on("click", function () {
+        $(".offcanvas-area").removeClass("opened");
+        $(".body-overlay").removeClass("opened");
     });
 })(jQuery);
